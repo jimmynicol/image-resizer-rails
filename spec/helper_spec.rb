fe417642f1331cca95a5189083b5599da069df0d
jimmynicol <james@fundly.com>
2014-08-10 19:35:23 -0700

    Updated specs to use newer expect syntax

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -17,7 +17,7 @@ describe 'Image::Resizer::Rails::Helper' do
 
   context 'no configuration set' do
     it 'should throw an exception if no CDN specified' do
-      -> { subject.ir_image_tag s3, s: 50 }.should raise_exception(
+      expect { subject.ir_image_tag s3, s: 50 }.to raise_exception(
         Image::Resizer::Rails::NoCDNException
       )
     end
@@ -32,33 +32,33 @@ describe 'Image::Resizer::Rails::Helper' do
     end
 
     it 'should not raise an exception when cdn set' do
-      -> { subject.ir_image_tag s3, s: 50 }.should_not raise_error
+      expect { subject.ir_image_tag s3, s: 50 }.not_to raise_error
     end
 
     it 'should build an s3 url correctly' do
-      subject.ir_url(s3).should eq "#{cdn}#{s3_obj}"
+      expect(subject.ir_url(s3)).to eq "#{cdn}#{s3_obj}"
     end
 
     it 'should build an inverse s3 url correctly' do
-      subject.ir_url(s3_inv).should eq "#{cdn}#{s3_obj}"
+      expect(subject.ir_url(s3_inv)).to eq "#{cdn}#{s3_obj}"
     end
 
     it 'should determine an s3 object correctly' do
-      subject.send(:s3_object, URI(s3)).should eq s3_obj
+      expect(subject.send(:s3_object, URI(s3))).to eq s3_obj
     end
 
     it 'should determine an object correctly from an inverse s3 url' do
-      subject.send(:s3_object, URI(s3_inv)).should eq s3_obj
+      expect(subject.send(:s3_object, URI(s3_inv))).to eq s3_obj
     end
 
     it 'should detect a Facebook url and set the endpoint correctly' do
       url = "#{cdn}/efacebook/missnine.jpg"
-      subject.ir_url(fb_url).should eq url
+      expect(subject.ir_url(fb_url)).to eq url
     end
 
     it 'should detect a Facebook url and set the modifiers correctly' do
       url = "#{cdn}/s50-efacebook/missnine.jpg"
-      subject.ir_url(fb_url, s: 50).should eq url
+      expect(subject.ir_url(fb_url, s: 50)).to eq url
     end
 
     it 'should respond to alias methods when they are set' do
@@ -67,9 +67,9 @@ describe 'Image::Resizer::Rails::Helper' do
         config.add_alias :ir_background, :sbg
         config.add_alias :ir_url, :surl
       end
-      subject.should respond_to :simagetag
-      subject.should respond_to :sbg
-      subject.should respond_to :surl
+      expect(subject).to respond_to :simagetag
+      expect(subject).to respond_to :sbg
+      expect(subject).to respond_to :surl
     end
 
     it 'should set alias methods appropriately' do
@@ -77,60 +77,60 @@ describe 'Image::Resizer::Rails::Helper' do
         config.add_alias :ir_url, :surl
       end
       url = "#{cdn}/s50-efacebook/missnine.jpg"
-      subject.ir_url(fb_url, s: 50).should eq url
-      subject.surl(fb_url, s: 50).should eq url
+      expect(subject.ir_url(fb_url, s: 50)).to eq url
+      expect(subject.surl(fb_url, s: 50)).to eq url
     end
 
     context 'set modifier strings correctly' do
       it 'should set square correctly' do
-        subject.ir_url(s3, s: 50).should eq "#{cdn}/s50#{s3_obj}"
+        expect(subject.ir_url(s3, s: 50)).to eq "#{cdn}/s50#{s3_obj}"
       end
       it 'should set height, width correctly' do
-        subject.ir_url(s3, h: 200, w: 300).should eq "#{cdn}/h200-w300#{s3_obj}"
+        expect(subject.ir_url(s3, h: 200, w: 300)).to eq "#{cdn}/h200-w300#{s3_obj}"
       end
       it 'should set crop properly' do
-        subject.ir_url(s3, c: 'fit').should eq "#{cdn}/cfit#{s3_obj}"
+        expect(subject.ir_url(s3, c: 'fit')).to eq "#{cdn}/cfit#{s3_obj}"
       end
       it 'should not set an invalid crop value' do
-        subject.ir_url(s3, c: 'else').should_not eq "#{cdn}/celse#{s3_obj}"
+        expect(subject.ir_url(s3, c: 'else')).to_not eq "#{cdn}/celse#{s3_obj}"
       end
       it 'should set gravity properly' do
-        subject.ir_url(s3, g: 'ne').should eq "#{cdn}/gne#{s3_obj}"
+        expect(subject.ir_url(s3, g: 'ne')).to eq "#{cdn}/gne#{s3_obj}"
       end
       it 'should not set an invalid gravity value' do
-        subject.ir_url(s3, g: 'else').should_not eq "#{cdn}/gelse#{s3_obj}"
+        expect(subject.ir_url(s3, g: 'else')).to_not eq "#{cdn}/gelse#{s3_obj}"
       end
     end
 
     context 'set external source correctly' do
       it 'should set facebook when fb_uid present' do
         url = "#{cdn}/efacebook/missnine.jpg"
-        subject.ir_url(fb_uid: 'missnine').should eq url
+        expect(subject.ir_url(fb_uid: 'missnine')).to eq url
       end
       it 'should set twitter when tw_uid present' do
         url = "#{cdn}/etwitter/djmissnine.jpg"
-        subject.ir_url(tw_uid: 'djmissnine').should eq url
+        expect(subject.ir_url(tw_uid: 'djmissnine')).to eq url
       end
       it 'should set youtube when tw_uid present' do
         url = "#{cdn}/eyoutube/3KIZUuvnQFY.jpg"
-        subject.ir_url(youtube_id: '3KIZUuvnQFY').should eq url
+        expect(subject.ir_url(youtube_id: '3KIZUuvnQFY')).to eq url
       end
       it 'should set vimeo when tw_uid present' do
         url = "#{cdn}/evimeo/69445362.jpg"
-        subject.ir_url(vimeo_id: '69445362').should eq url
+        expect(subject.ir_url(vimeo_id: '69445362')).to eq url
       end
     end
 
     context 'returning blank or nil sources' do
       it 'should return nil if nil source provided' do
-        subject.ir_image_tag(nil, s:50).should eq nil
-        subject.ir_url(nil, s:50).should eq nil
-        subject.ir_background(nil, s:50).should eq nil
+        expect(subject.ir_image_tag(nil, s:50)).to eq nil
+        expect(subject.ir_url(nil, s:50)).to eq nil
+        expect(subject.ir_background(nil, s:50)).to eq nil
       end
       it 'should return nil if blank source provided' do
-        subject.ir_image_tag('', s:50).should eq nil
-        subject.ir_url('', s:50).should eq nil
-        subject.ir_background('', s:50).should eq nil
+        expect(subject.ir_image_tag('', s:50)).to eq nil
+        expect(subject.ir_url('', s:50)).to eq nil
+        expect(subject.ir_background('', s:50)).to eq nil
       end
     end
   end
--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -10,50 +10,50 @@ describe 'Image::Resizer::Rails' do
     end
 
     it 'should have no cdn set' do
-      subject.cdn.should eq nil
+      expect(subject.cdn).to eq nil
     end
 
     it 'should set the cdn url' do
       subject.configure do |config|
         config.cdn = cdn
       end
-      subject.cdn.should eq cdn
+      expect(subject.cdn).to eq cdn
     end
 
     it 'should set a default for the js_class' do
-      subject.js_class.should eq 'ImageResizer'
+      expect(subject.js_class).to eq 'ImageResizer'
     end
 
     it 'should set the js_class' do
       subject.configure do |config|
         config.add_js_alias :js_class, 'IRAwesome'
       end
-      subject.js_class.should eq 'IRAwesome'
+      expect(subject.js_class).to eq 'IRAwesome'
     end
 
     it 'should set the js_image_tag' do
       subject.configure do |config|
         config.add_js_alias :js_image_tag, 'something_ir_tag'
       end
-      subject.js_image_tag.should eq 'something_ir_tag'
+      expect(subject.js_image_tag).to eq 'something_ir_tag'
     end
 
     it 'should set the js_background' do
       subject.configure do |config|
         config.add_js_alias :js_background, 'something_background'
       end
-      subject.js_background.should eq 'something_background'
+      expect(subject.js_background).to eq 'something_background'
     end
 
     it 'should set the js_url' do
       subject.configure do |config|
         config.add_js_alias :js_url, 'something_url'
       end
-      subject.js_url.should eq 'something_url'
+      expect(subject.js_url).to eq 'something_url'
     end
 
     it 'should include modifiers' do
-      subject.modifiers.should include(:w, :h, :s, :c, :g, :y, :x, :e, :f)
+      expect(subject.modifiers).to include(:w, :h, :s, :c, :g, :y, :x, :e, :f)
     end
 
     it 'should allow adding of new modifier' do
@@ -61,8 +61,8 @@ describe 'Image::Resizer::Rails' do
         config.add_modifier 'something', 'else'
       end
 
-      subject.modifiers.should include(:something)
-      subject.modifiers[:something][:alias].should eq 'else'
+      expect(subject.modifiers).to include(:something)
+      expect(subject.modifiers[:something][:alias]).to eq 'else'
     end
 
     it 'should allow adding of new source' do
@@ -70,7 +70,7 @@ describe 'Image::Resizer::Rails' do
         config.add_modifier 'something', 'else'
         config.add_source 'myspace', 'myspace_id'
       end
-      subject.modifiers[:e][:values].should include(:myspace)
+      expect(subject.modifiers[:e][:values]).to include(:myspace)
     end
   end
 
